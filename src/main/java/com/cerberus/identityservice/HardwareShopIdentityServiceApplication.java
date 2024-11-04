81b94006b8433e4b605627f4545738669dbac88d
egor <egormalov.2007@gmail.com>
2024-11-04 19:03:04 +1000

    Initial commit

---
+++ src/main/java/com/cerberus/identityservice/HardwareShopIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cerberus.identityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareShopIdentityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareShopIdentityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cerberus/identityservice/HardwareShopIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerberus.identityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareShopIdentityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
